3a2906cf03cab3cb82380f6e37fb809457488893
Carloshmp2697 <40127968+Carloshmp2697@users.noreply.github.com>
2018-06-09 23:21:33 -0600

    initial commit

---
+++ src/vista_administrador/Vista_Administrador.java
@@ -0,0 +1,14 @@
+
+package vista_administrador;
+
+
+public class Vista_Administrador {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana_Principal Inicio = new Ventana_Principal();
+        Inicio.setVisible(true);
+    }
+    
+}
